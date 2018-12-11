b9e910baba99134d4b3d4356ba61c036269dd98c
alessiocuomo <45040525+alessiocuomo@users.noreply.github.com>
2018-12-11 16:52:18 +0100

    o

--- UShop/src/Lista.java
+++ UShop/src/Lista.java
@@ -5,7 +5,29 @@ public class Lista {
 	ArrayList <Prodotto> list = new ArrayList <Prodotto> ();
 	
 	public void aggiungiProdotto() {
-		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci il nome del prodotto");
+		String nomeArticolo = in.nextLine();
+		Prodotto articolo = new Prodotto(nomeArticolo);
+		list.add(articolo);
+		}
+	
+	public void rimuoviProdotto() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserire il nome del prodotto da eliminare");
+		String nomeArticolo = in.nextLine();
+		for(int i = 0; i<list.size(); i++) {
+			if(list.get(i).equals(nomeArticolo)) {
+				
+			}
+		}
 	}
 	
+	public void visualizzaLista(){
+        System.out.println( list.size()+ " articoli. ");
+           for (Prodotto x : list) {
+               System.out.println(x.toString());
+               
+           }
+	}
 }
--- UShop/src/Prodotto.java
+++ UShop/src/Prodotto.java
@@ -1,19 +1,12 @@
 public class Prodotto {
 	private String nome;
-	private  int id;
-	private double prezzo;
-	private int quantita;
 	
-	
+	//Costruttore vuoto
 	public Prodotto() {
-		
 	}
-	
-	public Prodotto(String nome, int id, double prezzo) {
+	//Costruttore con nome
+	public Prodotto(String nome) {
 		this.nome = nome;
-		this.id = id;
-		this.prezzo = prezzo;
-		quantita = 100;
 	}
 
 	public String getNome() {
@@ -24,31 +17,4 @@ public class Prodotto {
 		this.nome = nome;
 	}
 
-	public int getID() {
-		return id;
-	}
-
-	public void setID(int id) {
-		id = id;
-	}
-
-	public double getPrezzo() {
-		return prezzo;
-	}
-
-	public void setPrezzo(double prezzo) {
-		this.prezzo = prezzo;
-	}
-
-	public int getQuantita() {
-		return quantita;
-	}
-
-	public void setQuantita(int quantita) {
-		this.quantita = quantita;
-	}
-	
-	
-	
-	
 }
