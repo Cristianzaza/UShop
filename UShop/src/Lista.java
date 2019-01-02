3f2912a42cf4ac84e5896a0fe983d1748785165b
Cristianzaza <cristian.zamparini@studenti.unicam.it>
2019-01-02 18:19:54 +0100

    modifiche a Lista e creazione main
    ho creato un main dove richiamo i metodi della classe Lista, l'unico che non riesco a far funzionare è il metodo rimuoviProdotto, la parte che non funziona è quella commentata, ovvero non risulta mai true la condizione "list.get(i).equals(prodDelete)". il nome del prodotto da eliminare l'ho dovuto mettere sotto forma di un Prodotto creato sul momento per fare il confronto in quanto il metodo equals si può fare con una stringa specifica o con un oggetto.

--- UShop/src/Lista.java
+++ UShop/src/Lista.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Lista {
 	ArrayList <Prodotto> list = new ArrayList <Prodotto> ();
@@ -16,17 +17,18 @@ public class Lista {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Inserire il nome del prodotto da eliminare");
 		String nomeArticolo = in.nextLine();
-		for(int i = 0; i<list.size(); i++) {
-			if(list.get(i).equals(nomeArticolo)) {
-				
-			}
-		}
+		Prodotto prodDelete = new Prodotto(nomeArticolo);
+		for (int i=0; i < list.size(); i++) {
+			/*if(list.get(i).equals(prodDelete))
+				System.out.println(prodDelete.getNome());
+              //  list.remove(i);*/
+        }
 	}
 	
 	public void visualizzaLista(){
         System.out.println( list.size()+ " articoli. ");
            for (Prodotto x : list) {
-               System.out.println(x.toString());
+               System.out.println(x.getNome());
                
            }
 	}
--- UShop/src/main.java
+++ UShop/src/main.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class main {
-	private static final Prodotto [] prodotti = {
-			new Prodotto("Latte", 00001, 1.50),
-			new Prodotto("Pane", 00002, 1.00),
-			new Prodotto("Carne", 00003, 3.00),
-			new Prodotto("Pasta", 00004, 0.80),
-			new Prodotto("Salmone", 00005, 4.80)
-	};
-	
-	public static void main(String[] args) {
-		Scanner in  = new Scanner(System.in);
-		System.out.println("Inserisci il nome del prodotto");
-		String nome = in.nextLine();
-		
-	}
+import java.util.List;
 
+public class main {
+	public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Lista nuovaLista = new Lista();
+    int userOpt = 0;
+    while (userOpt != 4) {
+        System.out.println("");
+        System.out.println("----- Lista della spesa ------");
+        System.out.println("(1) Inserisci un prodotto all'interno della lista. ");
+        System.out.println("(2) Mostra la lista salvata. ");
+        System.out.println("(3) Rimuovi un prodotto dalla lista. ");
+        System.out.println("(4) Esci. ");
+        userOpt = in.nextInt();  
+
+        if (userOpt == 1) {
+        nuovaLista.aggiungiProdotto();
+        }
+
+        if (userOpt == 2) {
+        nuovaLista.visualizzaLista();
+        }
+        
+        if (userOpt == 3) {
+        nuovaLista.rimuoviProdotto();
+        }
+    }
+}
 }
