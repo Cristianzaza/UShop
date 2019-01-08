4a4c07d5c3edc140de9139f9fd9f5befb9024bd5
Cristianzaza <cristian.zamparini@studenti.unicam.it>
2019-01-08 14:54:34 +0100

    Update Lista.java
    rimuoviProdotto funzionante

--- UShop/src/Lista.java
+++ UShop/src/Lista.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Lista {
 	ArrayList <Prodotto> list = new ArrayList <Prodotto> ();
@@ -12,16 +13,14 @@ public class Lista {
 		Prodotto articolo = new Prodotto(nomeArticolo);
 		list.add(articolo);
 		}
-	
 	public void rimuoviProdotto() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Inserire il nome del prodotto da eliminare");
 		String nomeArticolo = in.nextLine();
 		Prodotto prodDelete = new Prodotto(nomeArticolo);
 		for (int i=0; i < list.size(); i++) {
-			/*if(list.get(i).equals(prodDelete))
-				System.out.println(prodDelete.getNome());
-              //  list.remove(i);*/
+			if(list.get(i).getNome().equalsIgnoreCase(prodDelete.getNome()))
+              	list.remove(i);
         }
 	}
 	
