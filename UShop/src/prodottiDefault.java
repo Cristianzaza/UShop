7d94a7cca2e3c57c15bd699939a8fadbe1c629f9
Cristianzaza <cristian.zamparini@studenti.unicam.it>
2019-02-18 17:41:23 +0100

    c

---
+++ UShop/src/prodottiDefault.java
@@ -0,0 +1,6 @@
+
+public enum prodottiDefault {
+	Acqua, Formaggio, Carne, Pane, Frutta, Verdura, Latte, Yogurt,
+	Sugo, Pasta, Bibite, Merendine, Biscotti, Birra, Vino, Surgelati, 
+	Detersivi, Carta_igienica, Igiene_personale;
+}
